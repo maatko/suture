c3afaf079865ed2a2a1a160a55c600b6b3f0c01c
maatko <matkovukovic25@gmail.com>
2026-04-22 17:58:03 +0200

    moved the bswap outside of public api into the stream

--- src/stream.c
+++ src/stream.c
@@ -1,6 +1,27 @@
 #include <suture/stream.h>
-
 #include <string.h>
+
+#if defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#  define bswap_16(x) (x)
+#  define bswap_32(x) (x)
+#  define bswap_64(x) (x)
+#elif defined(__clang__) || (defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 3)))
+#  define bswap_16(x) __builtin_bswap16(x)
+#  define bswap_32(x) __builtin_bswap32(x)
+#  define bswap_64(x) __builtin_bswap64(x)
+#elif defined(_MSC_VER)
+#  include <stdlib.h>
+#  define bswap_16(x) _byteswap_ushort(x)
+#  define bswap_32(x) _byteswap_ulong(x)
+#  define bswap_64(x) _byteswap_uint64(x)
+#elif defined(__APPLE__)
+#  include <libkern/OSByteOrder.h>
+#  define bswap_16(x) OSSwapInt16(x)
+#  define bswap_32(x) OSSwapInt32(x)
+#  define bswap_64(x) OSSwapInt64(x)
+#else
+#  include <byteswap.h>
+#endif
 
 enum su_error su_stream_rn(struct su_stream *stream, u1 *buffer, const u2 size, const u2 offset) {
   if (stream == NULL || buffer == NULL)
